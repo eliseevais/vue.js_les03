bfca2ee46dbea7041625b484916df6081c1b92ec
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2022-11-06 18:01:34 +0300

    loading homework 03

---
+++ src/components/AddNewCost.vue
@@ -0,0 +1,33 @@
+<template>
+  <div @click="onBtnClick" class="btn">add new cost +</div>
+</template>
+
+<script>
+export default {
+  name: "AddNewCost",
+  methods: {
+    onBtnClick() {
+      this.$emit('showForm');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  width: 150px;
+  background-color: #24aca0;
+  border: 1px solid ;
+  border-radius: 3px;
+  color: white;
+  padding: 7px 25px;
+  margin: 10px auto 20px;
+  cursor: pointer;
+  transition: 0.3s;
+  text-transform: uppercase;
+  &:hover {
+    background-color: transparent;
+    color: #24aca0;
+  }
+}
+</style>
